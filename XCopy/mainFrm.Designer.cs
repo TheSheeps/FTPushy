186765252b380224a3aa28dbec3fca8dfac05422
armin <armin@msi.local>
2015-04-25 04:12:34 +0430

    first commit

---
+++ XCopy/mainFrm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace XCopy
+{
+    partial class mainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.settingLbl = new System.Windows.Forms.LinkLabel();
+            this.statusLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // settingLbl
+            // 
+            this.settingLbl.ActiveLinkColor = System.Drawing.Color.Black;
+            this.settingLbl.AutoSize = true;
+            this.settingLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.settingLbl.LinkColor = System.Drawing.Color.Black;
+            this.settingLbl.Location = new System.Drawing.Point(211, 9);
+            this.settingLbl.Name = "settingLbl";
+            this.settingLbl.Size = new System.Drawing.Size(40, 13);
+            this.settingLbl.TabIndex = 0;
+            this.settingLbl.TabStop = true;
+            this.settingLbl.Text = "Setting";
+            this.settingLbl.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.settingLbl_LinkClicked);
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.AutoSize = true;
+            this.statusLbl.Location = new System.Drawing.Point(52, 48);
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(40, 13);
+            this.statusLbl.TabIndex = 1;
+            this.statusLbl.Text = "Status:";
+            // 
+            // mainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 109);
+            this.Controls.Add(this.statusLbl);
+            this.Controls.Add(this.settingLbl);
+            this.Name = "mainFrm";
+            this.Text = "XCopy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel settingLbl;
+        private System.Windows.Forms.Label statusLbl;
+    }
+}
+
---
+++ XCopy/settingFrm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace XCopy
+{
+    partial class settingFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fromLbl = new System.Windows.Forms.Label();
+            this.toLbl = new System.Windows.Forms.Label();
+            this.fromTxtBox = new System.Windows.Forms.TextBox();
+            this.toTxtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // fromLbl
+            // 
+            this.fromLbl.AutoSize = true;
+            this.fromLbl.Location = new System.Drawing.Point(12, 29);
+            this.fromLbl.Name = "fromLbl";
+            this.fromLbl.Size = new System.Drawing.Size(57, 13);
+            this.fromLbl.TabIndex = 0;
+            this.fromLbl.Text = "Copy from:";
+            // 
+            // toLbl
+            // 
+            this.toLbl.AutoSize = true;
+            this.toLbl.Location = new System.Drawing.Point(12, 93);
+            this.toLbl.Name = "toLbl";
+            this.toLbl.Size = new System.Drawing.Size(46, 13);
+            this.toLbl.TabIndex = 1;
+            this.toLbl.Text = "Copy to:";
+            // 
+            // fromTxtBox
+            // 
+            this.fromTxtBox.Location = new System.Drawing.Point(75, 26);
+            this.fromTxtBox.Name = "fromTxtBox";
+            this.fromTxtBox.Size = new System.Drawing.Size(235, 20);
+            this.fromTxtBox.TabIndex = 2;
+            // 
+            // toTxtBox
+            // 
+            this.toTxtBox.Location = new System.Drawing.Point(75, 90);
+            this.toTxtBox.Name = "toTxtBox";
+            this.toTxtBox.Size = new System.Drawing.Size(235, 20);
+            this.toTxtBox.TabIndex = 3;
+            // 
+            // settingFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 140);
+            this.Controls.Add(this.toTxtBox);
+            this.Controls.Add(this.fromTxtBox);
+            this.Controls.Add(this.toLbl);
+            this.Controls.Add(this.fromLbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "settingFrm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Setting";
+            this.Load += new System.EventHandler(this.settingFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fromLbl;
+        private System.Windows.Forms.Label toLbl;
+        private System.Windows.Forms.TextBox fromTxtBox;
+        private System.Windows.Forms.TextBox toTxtBox;
+    }
+}
